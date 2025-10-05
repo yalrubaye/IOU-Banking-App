07a3e8f824078b8ced43e4775506fa29747c9713
Yousif Alrubaye <yousif.alrubaye1994@gmail.com>
2025-10-04 22:12:35 -0700

    Update bbaDatabase.java

--- bbaDatabase.java
+++ bbaDatabase.java
@@ -14,7 +14,7 @@ public class bbaDatabase {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
     
-    //TEST FUNCTION
+    //test FUNCTION
     /*
     //here is a function to check if a user name exists in the 'users' table that was used for testing
     public static boolean checkUsername(String username) {
